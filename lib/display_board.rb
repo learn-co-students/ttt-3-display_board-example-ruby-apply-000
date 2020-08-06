2fe2f8643d6c6b3d141c84fdcc3883bb010cf2a4
Nicholas Parke <nickparke2020@gmail.com>
2020-08-06 19:17:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,8 @@
 def display_board
-  print "   |   "
-  print "   |   |  "
-  print "-----------"
-  print 'prints a 3x3 tic tac toe board'
-  print = "   |   |   \n"
-end
-
-'prints a 3x3 tic tac toe board'
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+ end
+ 
