5a4f0969b7fe1c8434004e2b74ae30e620b96c53
Elvis Landi <elandi13@yahoo.com>
2020-09-25 18:48:38 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
-Def > Display Board
-puts " A Tic Tac Toe Board "
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+def display_board(board)
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
